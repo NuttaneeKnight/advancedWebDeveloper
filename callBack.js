867c8661b79221d956771c89146ad30dd2c71a9a
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-12-08 19:43:41 -0500

    forEach

--- callBack.js
+++ callBack.js
@@ -7,71 +7,81 @@
  * Create callbacks using anonymous functions
  */
 
-function callback() {
-  console.log("Coming from callback");
+// function callback() {
+//   console.log("Coming from callback");
+// }
+
+// function higherOrder(fn) {
+//   console.log("About to call callback");
+//   fn();
+//   console.log("Callback has been invoked");
+// }
+// // node callback.js
+// higherOrder(callback);
+
+// /**
+//  * What are callbacks used for?
+//  * - Advanced Array Methods
+//  * - Browser events
+//  * - AJAX Requests
+//  * - React Development
+//  */
+
+// // Codes without callbacks
+// function sendMessageConsole(message) {
+//   console.log(message);
+// }
+
+// function sendMessageAlert(message) {
+//   alert(message);
+// }
+
+// function sendMessageConfirm(message) {
+//   return confirm(message);
+// }
+
+// sendMessageAlert("Lots of duplication");
+
+// // Same code refactor with callbacks
+// // This is a higheroerder function
+// function sendMessage(message, callback) {
+//   return callback(message);
+// }
+
+// sendMessage("Message for console", console.log);
+
+// sendMessage("Message for alert", alert);
+
+// var answer = sendMessage("Are you sure??", confirm);
+
+// // Callbacks with Function Declarations
+// function greet(name, formatter) {
+//   return "Hello, " + formatter(name);
+// }
+
+// function upperCaseName(name) {
+//   return name.toUpperCase();
+// }
+
+// greet("Tim", upperCaseName);
+
+// // Callbacks With Anonymous function Functions
+// function greet(name, formatter) {
+//   return "Hello, " + formatter(name);
+// }
+// greet("TIm", function (name) {
+//   return name.toUppercase();
+// });
+// greet("Tim", function (name) {
+//   return name + "!!!!!";
+// });
+
+// Print Array Values Doubled
+
+var arr = [1, 2, 3, 4, 5, 6];
+function double(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i] *2 )
+  }
 }
-
-function higherOrder(fn) {
-  console.log("About to call callback");
-  fn();
-  console.log("Callback has been invoked");
-}
-// node callback.js
-higherOrder(callback);
-
-/**
- * What are callbacks used for?
- * - Advanced Array Methods
- * - Browser events
- * - AJAX Requests
- * - React Development
- */
-
-// Codes without callbacks
-function sendMessageConsole(message) {
-  console.log(message);
-}
-
-function sendMessageAlert(message) {
-  alert(message);
-}
-
-function sendMessageConfirm(message) {
-  return confirm(message);
-}
-
-sendMessageAlert("Lots of duplication");
-
-// Same code refactor with callbacks
-// This is a higheroerder function
-function sendMessage(message, callback) {
-  return callback(message);
-}
-
-sendMessage("Message for console", console.log);
-
-sendMessage("Message for alert", alert);
-
-var answer = sendMessage("Are you sure??", confirm);
-
-// Callbacks with Function Declarations
-function greet(name, formatter) {
-  return "Hello, " + formatter(name);
-}
-
-function upperCaseName(name) {
-  return name.toUpperCase();
-}
-
-greet("Tim", upperCaseName);
-
-// Callbacks With Anonymous function Functions
-function greet(name, formatter) {
-  return "Hello, " + formatter(name)
-}
-greet("TIm", function(name) {
-  return name.toUppercase()
-})
-greet("Tim", function(name) {
-  return name + "!!!!!"
-})
+double(arr)
