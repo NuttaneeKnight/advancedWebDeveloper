4f857a4a2fc79728167c020052f646c8b6ef2f96
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-12-08 20:01:14 -0500

    note

--- callBack.js
+++ callBack.js
@@ -76,32 +76,40 @@
 //   return name + "!!!!!";
 // });
 
-// Print Array Values Doubled
-var arr = [1, 2, 3, 4, 5, 6];
-function double(arr) {
+// // Print Array Values Doubled
+// var arr = [1, 2, 3, 4, 5, 6];
+// function double(arr) {
+//   for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i] *2 )
+//   }
+// }
+// double(arr)
+
+// // Refactor with forEach
+// // Callback signature
+// function callback(curElement, currentIndex, array) {
+//   //implement forEach
+// }
+// forEach(arr, function(arr) {
+//   console.log(number * 2)
+// })
+
+// // forEach Example with all callback PArameters
+// var strings = ['my', 'forEach', 'example']
+
+// var result = ''
+// forEach(strings, function(str, index, array) {
+//   if (array.length -1 !== index) {
+//     result += str + ' '
+//   } else {
+//     result += str + "!!!"
+//   }
+// }) // 'my forEach example!!!'
+
+
+// Sample
+function forEach(arr, callback) {
   for (var i = 0; i < arr.length; i++) {
-    console.log(arr[i] *2 )
+    callback(arr[i], i, arr)
   }
 }
-double(arr)
-
-// Refactor with forEach
-// Callback signature
-function callback(curElement, currentIndex, array) {
-  //implement forEach
-}
-forEach(arr, function(arr) {
-  console.log(number * 2)
-})
-
-// forEach Example with all callback PArameters
-var strings = ['my', 'forEach', 'example']
-
-var result = ''
-forEach(strings, function(str, index, array) {
-  if (array.length -1 !== index) {
-    result += str + ' '
-  } else {
-    result += str + "!!!"
-  }
-})
