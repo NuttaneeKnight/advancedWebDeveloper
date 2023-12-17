7d9179d64b7ce3012c5e526fa062601e25ce003e
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-12-16 19:48:48 -0500

    find index

--- callBack.js
+++ callBack.js
@@ -130,3 +130,11 @@ findIndex(arr, function(num, index, array) {
   return num === 6;
 })
 
+// ex. findIndex, find first even 
+var arr = [5, 11, 13, 8, 6, 7];
+findIndex(arr, function(num, index, array) {
+  return num % 2 === 0
+})
+
+
+
