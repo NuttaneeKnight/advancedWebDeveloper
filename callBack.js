eec631564f065cd099b242224406fa85ab3a0c6f
Nuttanee Knight <nuttaneeknight@gmail.com>
2024-02-28 20:10:44 -0500

    ajax bitcoin

--- callBack.js
+++ callBack.js
@@ -496,6 +496,23 @@ span {
 }
 
 JS:
+var btn = document.querySelector('button');
+var priceDisp = document.querySelector('#price');
 
+btn.addEventListener('click', function(){
+  var XHR = new XMLHttpRequest();
+  
+  XHR.onreadystatechange = function () {
+    if (XHR.readyState == 4 && XHR.status == 200) {
+      var data = JSON.parse(XHR.responseText)
+      var price = data.bpi.GBP.rate
+      priceDisp.innerText = price + ' GBP'; 
+    }
+  }
+  
+  var url = "https://api.coindesk.com/v1/bpi/currentprice.json";  
+  XHR.open("GET", url);
+  XHR.send();
+})
 
 */
